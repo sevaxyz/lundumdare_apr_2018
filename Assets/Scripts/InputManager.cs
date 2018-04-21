e6c11db90e891e5b6ab47d31f50ec1187dde61f7
Yury <yuriykurin@gmail.com>
2018-04-21 21:23:33 +0300

    Created InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("A") || Input.GetKeyDown("ф") || Input.GetKeyDown("Ф"))
+        {
+
+        }
+        else if (Input.GetKeyDown("s") || Input.GetKeyDown("S") || Input.GetKeyDown("ы") || Input.GetKeyDown("Ы"))
+        {
+
+        }
+        else if (Input.GetKeyDown("d") || Input.GetKeyDown("D") || Input.GetKeyDown("в") || Input.GetKeyDown("В"))
+        {
+
+        }
+    }
+}
