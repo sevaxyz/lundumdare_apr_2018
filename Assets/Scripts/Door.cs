f073674f795fa467a2a286d76746c1ee9666c122
Yury <yuriykurin@gmail.com>
2018-04-22 12:26:01 +0300

    Created Guy and Door scripts

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject guyPrefab;
+    private Guy guy = null;
+
+    public bool IsOccupied { set; get; }
+
+    // Use this for initialization
+    void Start ()
+    {
+        IsOccupied = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    public void SpawnGuy()
+    {
+        if (!IsOccupied)
+        {
+            // TODO Create Guy With GuyGenerator
+            var guyGameObj = Instantiate(guyPrefab);
+            guy = guyGameObj.GetComponent<Guy>();
+            IsOccupied = true;
+        }
+    }
+
+    public void KillGuy()
+    {
+        if (IsOccupied && guy)
+        {
+            guy.KillSelf();
+            guy = null;
+            IsOccupied = false;
+        }
+    }
+}
